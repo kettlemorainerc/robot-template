04902124aa70dbc4ae2af7eba78718d2e166a954
SquEdgy <dafman6@outlook.com>
2023-01-28 14:07:58 -0600

    Update common files to (hopefully) be more betterer, Update RobotHardware to be less cluttered and handle updated stuff better.

--- src/main/java/org/usfirst/frc/team2077/RobotHardware.java
+++ src/main/java/org/usfirst/frc/team2077/RobotHardware.java
@@ -1,10 +1,47 @@
 package org.usfirst.frc.team2077;
 
-import org.usfirst.frc.team2077.common.HardwareRequirements;
+import org.usfirst.frc.team2077.common.*;
 
+import java.util.EnumMap;
+/*
+Before implementing any inherited methods (methods with the "abstract" modifier in HardwareRequirements)
+You should replace "DRIVE_MODULE" with the type of modules your chassis uses.
+Ex. SparkNeoDriveModule for mecanums, SwerveModule for swerve, etc.
+and replace "CHASSIS_TYPE" with the type of chassis you're using MecanumChassis, SwerveChassis, etc.
+*/
 
 /**
- * This is intended to be a spot for the definition and retrieval of all robot specific hardware
+ * This is intended to be a spot for the definition and retrieval of all robot hardware.
  * */
-public class RobotHardware implements HardwareRequirements</*DRIVE_MODULE*/, /*CHASSIS_TYPE*/> {
+public class RobotHardware implements HardwareRequirements<DRIVE_MODULE, CHASSIS_TYPE> {
+    private final CHASSIS_TYPE chassis;
+    private final Map<WheelPosition, DRIVE_MODULE> wheels = new EnumMap<>(WheelPosition.class);
+
+    public RobotHardware() {
+        super();
+
+        for(WheelPosition p : WheelPosition.values()) {
+            wheels.put(p, makeWheel(p));
+        }
+
+        chassis = new ChassisType(this);
+    }
+
+    private DRIVE_MODULE makeWheel(WheelPosition position) {
+        switch(position) {
+            case FRONT_LEFT -> new DRIVE_MODULE();
+            case FRONT_RIGHT -> new DRIVE_MODULE();
+            case BACK_LEFT -> new DRIVE_MODULE();
+            case BACK_RIGHT -> new DRIVE_MODULE();
+        }
+        throw new IllegalArgumentException("Unsupported Wheel position: " + position);
+    }
+
+    @Override public CHASSIS_TYPE getChassis() {
+        return chassis;
+    }
+
+    @Override public DRIVE_MODULE getWheel(WheelPosition pos) {
+        return wheels.get(pos);
+    }
 }
